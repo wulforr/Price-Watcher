63908cb730c9344766a6378ee8af7283da40580c
wulforr <gsshaurya@gmail.com>
2020-06-29 02:14:45 +0530

    changed Line to Area CHart

--- price-matcher/src/components/SingleWatcher.js
+++ price-matcher/src/components/SingleWatcher.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip,AreaChart,Area } from 'recharts';
 import moment from 'moment'
 
 export default function SingleWatcher(props) {
@@ -11,13 +11,20 @@ export default function SingleWatcher(props) {
   console.log(data)
   // const Xticks = data.map(ele => ele.date)
   const renderLineChart = (
-    <LineChart width={600} height={300} data={dates} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-      <Line type="monotone" dataKey="price" stroke="#06d69e" />
+    <AreaChart width={600} height={300} data={dates} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+      {/* <Line type="monotone" dataKey="price" stroke="#06d69e" /> */}
+      <defs>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
       {/* <CartesianGrid stroke="#ccc" strokeDasharray="5 5" /> */}
       <XAxis dataKey="date" stroke='#333' />
       <YAxis stroke='#333' />
+      <Area type="monotone" dataKey="price" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
       <Tooltip />
-    </LineChart>
+    </AreaChart>
   );
   return (
     <div style={{padding:'30px'}}>
